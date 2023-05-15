a1cec9241bf5f77ffbf1e5c39b2582ad4b2a0a80
yThanos <vitorfraporti@hotmail.com>
2023-05-15 15:29:36 -0300

    ok

--- lib/screens/android/home.dart
+++ lib/screens/android/home.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/screens/android/despesas.dart';
 import 'package:flutter_app/screens/android/grupos.dart';
-import 'package:flutter_app/screens/android/inicio.dart';
 import 'package:flutter_app/screens/android/lista.dart';
 
+import '../../widgets/bordaredonda.dart';
 import 'login.dart';
 
 class home extends StatefulWidget {
@@ -18,11 +18,168 @@ int _selected = 0;
 class _homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
-
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     List<Widget> _lsita = [
-      inicio(),
-      grupos(),
+      Center(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(20, 60, 20, 100),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: screenHeight * 0.3,
+                      width: screenWidth * 0.80,
+                      decoration: bordaRendonda(),
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                                    child: Text("0", style: TextStyle(fontSize: 34),),
+                                  ),
+                                  SizedBox(width: 10,),
+                                  Text("Despesas!", style: TextStyle(fontSize: 22),),
+                                ],
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text("Totalizando: ", style: TextStyle(fontSize: 22),),
+                                  SizedBox(width: 10,),
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                                    child: Text("R\$ 00.00", style: TextStyle(fontSize: 34),),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          Positioned(
+                            bottom: 10,
+                            left: 0,
+                            right: 0,
+                            child: Center(
+                              child: SizedBox(
+                                height: 40,
+                                width: 100,
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _selected = 1;
+                                    });
+                                  },
+                                  child: Text('Despesas'),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: screenHeight * 0.05,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: screenHeight * 0.3,
+                      width: screenWidth * 0.80,
+                      decoration: bordaRendonda(),
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                                    child: Text("0", style: TextStyle(fontSize: 34),),
+                                  ),
+                                  SizedBox(width: 10,),
+                                  Text("Despesas", style: TextStyle(fontSize: 22),),
+                                ],
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text("em", style: TextStyle(fontSize: 22),),
+                                  SizedBox(width: 10,),
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                                    child: Text("0", style: TextStyle(fontSize: 34),),
+                                  ),
+                                  SizedBox(width: 10,),
+                                  Text("grupos", style: TextStyle(fontSize: 22),),
+                                ],
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text("Totalizando: ", style: TextStyle(fontSize: 22),),
+                                  SizedBox(width: 10,),
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                                    child: Text("R\$ 00.00", style: TextStyle(fontSize: 34),),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          Positioned(
+                            bottom: 10,
+                            left: 0,
+                            right: 0,
+                            child: Center(
+                              child: SizedBox(
+                                height: 40,
+                                width: 100,
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _selected = 2;
+                                    });
+                                  },
+                                  child: Text('Grupos'),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )),
       despesas(),
+      grupos(),
       lista()
     ];
     return Scaffold(
@@ -67,11 +224,11 @@ class _homeState extends State<home> {
               label: "Home",
               backgroundColor: Colors.lightGreenAccent),
           BottomNavigationBarItem(
-              icon: Icon(Icons.group),
+              icon: Icon(Icons.attach_money),
               label: "Despesas",
               backgroundColor: Colors.lightGreenAccent),
           BottomNavigationBarItem(
-              icon: Icon(Icons.attach_money),
+              icon: Icon(Icons.group),
               label: "Grupos",
               backgroundColor: Colors.lightGreenAccent),
           BottomNavigationBarItem(
